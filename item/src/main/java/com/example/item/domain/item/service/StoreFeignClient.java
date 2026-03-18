d463d19f7f4129ec1105a13c33894163ba19c20e
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-03-18 19:15:03 +0900

    [SweetDeal] refactor: Api -> ResponseEntity 대체

--- item/src/main/java/com/example/item/domain/item/service/StoreFeignClient.java
+++ item/src/main/java/com/example/item/domain/item/service/StoreFeignClient.java
@@ -1,8 +1,8 @@
 package com.example.item.domain.item.service;
 
-import com.example.global.api.Api;
 import com.example.item.domain.item.controller.model.response.StoreSimpleResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -10,5 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface StoreFeignClient {
 
   @GetMapping(value = "/store/{userId}", headers = "X-Internal=true")
-  Api<StoreSimpleResponse> getStores(@PathVariable Long userId);
+  ResponseEntity<StoreSimpleResponse> getStores(@PathVariable Long userId);
 }
+
