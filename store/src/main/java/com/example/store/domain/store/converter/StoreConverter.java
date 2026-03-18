ac62af908886e8a21f6e619f25c087300618f1b5
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-03-18 19:25:22 +0900

    [SweetDeal] refactor: 불필요한 메서드 삭제 및 DTO record로 변경에 따른 메서드 호출 수정

--- store/src/main/java/com/example/store/domain/store/converter/StoreConverter.java
+++ store/src/main/java/com/example/store/domain/store/converter/StoreConverter.java
@@ -1,31 +1,32 @@
 package com.example.store.domain.store.converter;
 
-import com.example.global.anntation.Converter;
+import com.example.global.annotation.Converter;
 import com.example.store.domain.store.controller.model.response.NearbyStoreResponse;
+import com.example.store.domain.store.controller.model.response.NearbyStoresResponse;
 import com.example.store.domain.store.controller.model.request.StoreRegisterRequest;
+import com.example.store.domain.store.controller.model.response.OwnerStoreResponse;
 import com.example.store.domain.store.controller.model.response.OwnerStoresResponse;
 import com.example.store.domain.store.controller.model.response.StoreNameKeywordResponse;
-import com.example.store.domain.store.controller.model.response.StoreRegisterResponse;
 import com.example.store.domain.store.controller.model.response.StoreSimpleResponse;
+import com.example.store.domain.store.controller.model.response.StoresNameKeywordResponse;
 import com.example.store.domain.store.controller.model.response.UserStoreResponse;
 import com.example.store.domain.store.repository.Store;
 import com.example.store.domain.store.repository.enums.OperatingStatus;
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Converter
 public class StoreConverter {
 
-  public Store toEntity(StoreRegisterRequest storeRegisterRequest, Long userId) {
+  public Store toEntity(StoreRegisterRequest request, Long userId) {
     Store store = Store.builder()
         .userId(userId)
-        .name(storeRegisterRequest.getName())
-        .address(storeRegisterRequest.getAddress())
-        .phone(storeRegisterRequest.getPhone())
-        .businessNumber(storeRegisterRequest.getBusinessNumber())
-        .category(storeRegisterRequest.getCategory())
-        .operatingTime(storeRegisterRequest.getOperatingTime())
+        .name(request.getName())
+        .address(request.getAddress())
+        .phone(request.getPhone())
+        .businessNumber(request.getBusinessNumber())
+        .category(request.getCategory())
+        .operatingTime(request.getOperatingTime())
         .build();
 
     if (store.isOperating()) {
@@ -34,25 +35,8 @@ public class StoreConverter {
     return store;
   }
 
-  public StoreRegisterResponse toResponse(Store store) {
-    return StoreRegisterResponse.builder()
-        .id(store.getId())
-        .name(store.getName())
-        .address(store.getAddress())
-        .phone(store.getPhone())
-        .businessNumber(store.getBusinessNumber())
-        .category(store.getCategory())
-        .storeStatus(store.getStoreStatus())
-        .registeredAt(store.getRegisteredAt())
-        .openingTime(store.getOperatingTime().getOpeningTime())
-        .closingTime(store.getOperatingTime().getClosingTime())
-        .operatingStatus(store.getOperatingStatus())
-        .build();
-
-  }
-
-  public List<NearbyStoreResponse> toNearbyStoreResponse(List<Store> stores, BigDecimal radiusInMeters) {
-    return stores.stream().map(
+  public NearbyStoresResponse toNearbyStoreResponse(List<Store> stores, BigDecimal radiusInMeters) {
+    List<NearbyStoreResponse> nearbyStoreResponses = stores.stream().map(
         store -> NearbyStoreResponse.builder()
             .id(store.getId())
             .sido(store.getAddress().getSido())
@@ -62,21 +46,23 @@ public class StoreConverter {
             .longitude(store.getAddress().getLongitude())
             .radiusInMeters(radiusInMeters)
             .build()
-    ).collect(Collectors.toList());
+    ).toList();
 
+    return NearbyStoresResponse.builder()
+        .nearbyStoreResponses(nearbyStoreResponses)
+        .build();
   }
 
   public StoreSimpleResponse toStoreSimpleResponse(List<Store> stores) {
-    List<Long> storeId = stores.stream().map(store -> store.getId()).toList();
+    List<Long> storesId = stores.stream().map(store -> store.getId()).toList();
 
-    StoreSimpleResponse storeSimpleResponse = new StoreSimpleResponse();
-    storeSimpleResponse.setStoresId(storeId);
+    StoreSimpleResponse storeSimpleResponse = new StoreSimpleResponse(storesId);
 
     return storeSimpleResponse;
   }
 
-  public List<StoreNameKeywordResponse> toStoreNameKeywordResponse(List<Store> stores) {
-    return stores.stream().map(
+  public StoresNameKeywordResponse toStoreNameKeywordResponse(List<Store> stores) {
+    List<StoreNameKeywordResponse> storeNameKeywordResponses = stores.stream().map(
         store -> StoreNameKeywordResponse.builder()
             .storeId(store.getId())
             .name(store.getName())
@@ -85,13 +71,16 @@ public class StoreConverter {
             .operatingStatus(store.getOperatingStatus())
             .operatingTime(store.getOperatingTime())
             .build()
-    ).collect(Collectors.toList());
+    ).toList();
 
+    return StoresNameKeywordResponse.builder()
+        .storeNameKeywordResponses(storeNameKeywordResponses)
+        .build();
   }
 
-  public List<OwnerStoresResponse> toOwnerStoresResponse(List<Store> stores) {
-    return stores.stream().map(
-        store -> OwnerStoresResponse.builder()
+  public OwnerStoresResponse toOwnerStoresResponse(List<Store> stores) {
+    List<OwnerStoreResponse> ownerStoreResponses = stores.stream().map(
+        store -> OwnerStoreResponse.builder()
             .storeId(store.getId())
             .name(store.getName())
             .address(store.getAddress())
@@ -101,7 +90,11 @@ public class StoreConverter {
             .status(store.getStoreStatus())
             .registeredAt(store.getRegisteredAt())
             .build()
-    ).collect(Collectors.toList());
+    ).toList();
+
+    return OwnerStoresResponse.builder()
+        .ownerStoreResponses(ownerStoreResponses)
+        .build();
 
   }
 
