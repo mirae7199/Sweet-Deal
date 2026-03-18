50b51f0f8af51251c7da24f704205b2216167005
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-03-18 19:15:19 +0900

    [SweetDeal] refactor: Api -> ResponseEntity 대체

--- store/src/main/java/com/example/store/domain/store/controller/StoreInternalApiController.java
+++ store/src/main/java/com/example/store/domain/store/controller/StoreInternalApiController.java
@@ -1,9 +1,9 @@
 package com.example.store.domain.store.controller;
 
-import com.example.global.api.Api;
 import com.example.store.domain.store.business.StoreBusiness;
 import com.example.store.domain.store.controller.model.response.StoreSimpleResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +17,9 @@ public class StoreInternalApiController {
   private final StoreBusiness storeBusiness;
 
   @GetMapping("/store/{userId}")
-  public Api<StoreSimpleResponse> getStore(@PathVariable Long userId) {
+  public ResponseEntity<StoreSimpleResponse> getStore(@PathVariable Long userId) {
     StoreSimpleResponse storeSimpleResponse = storeBusiness.getSimpleStore(userId);
-    return Api.ok(storeSimpleResponse);
+    return ResponseEntity.ok(storeSimpleResponse);
   }
 
 }
