acaf6252ec22b38cde182ec03b8e47d8bcb275a6
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-03-18 19:19:05 +0900

    [SweetDeal] refactor: class -> record로 변경 및 정적 팩토링 메서드 추가 구현

--- store/src/main/java/com/example/store/domain/store/controller/model/request/UpdateImageRequest.java
+++ store/src/main/java/com/example/store/domain/store/controller/model/request/UpdateImageRequest.java
@@ -2,19 +2,29 @@ package com.example.store.domain.store.controller.model.request;
 
 import com.example.store.domain.store.repository.enums.ImageKind;
 import java.util.List;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class UpdateImageRequest {
+public record UpdateImageRequest(
+    Long itemId,
+    Long storeId,
+    ImageKind imageKind,
+    List<String> serverName
+  ) {
 
-  private Long itemId;
-  private Long storeId;
-  private ImageKind imageKind;
-  private List<String> serverName;
+  public static UpdateImageRequest of(
+      Long itemId,
+      Long storeId,
+      ImageKind imageKind,
+      List<String> serverName
+  ) {
+    return UpdateImageRequest.builder()
+        .itemId(itemId)
+        .storeId(storeId)
+        .imageKind(imageKind)
+        .serverName(serverName)
+        .build();
+  }
+
+
 }
