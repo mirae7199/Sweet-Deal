961ac27137d8c7888e15aabb72e7972182f3b297
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-03-18 19:13:43 +0900

    [SweetDeal] refactor: messageConverter 삭제, 가독성 향상, BusinessException 예외 처리 구현

--- store/src/main/java/com/example/store/domain/store/business/StoreBusiness.java
+++ store/src/main/java/com/example/store/domain/store/business/StoreBusiness.java
@@ -1,21 +1,18 @@
 package com.example.store.domain.store.business;
 
-import com.example.global.anntation.Business;
+import com.example.global.annotation.Business;
 import com.example.global.errorcode.StoreErrorCode;
-import com.example.store.domain.common.exception.IsBlankException;
+import com.example.global.exception.BusinessException;
 import com.example.store.domain.store.controller.model.request.RegisterImageRequest;
 import com.example.store.domain.store.controller.model.request.UpdateImageRequest;
-import com.example.store.domain.store.controller.model.response.NearbyStoreResponse;
+import com.example.store.domain.store.controller.model.response.NearbyStoresResponse;
 import com.example.store.domain.store.controller.model.request.LocationRequest;
 import com.example.store.domain.store.controller.model.request.StoreRegisterRequest;
 import com.example.store.domain.store.controller.model.request.StoreUpdateRequest;
-import com.example.store.domain.store.controller.model.response.MessageResponse;
 import com.example.store.domain.store.controller.model.response.OwnerStoresResponse;
-import com.example.store.domain.store.controller.model.response.StoreNameKeywordResponse;
-import com.example.store.domain.store.controller.model.response.StoreRegisterResponse;
 import com.example.store.domain.store.controller.model.response.StoreSimpleResponse;
+import com.example.store.domain.store.controller.model.response.StoresNameKeywordResponse;
 import com.example.store.domain.store.controller.model.response.UserStoreResponse;
-import com.example.store.domain.store.converter.MessageConverter;
 import com.example.store.domain.store.converter.StoreConverter;
 import com.example.store.domain.store.repository.Store;
 import com.example.store.domain.store.repository.enums.ImageKind;
@@ -24,84 +21,115 @@ import com.example.store.domain.store.repository.enums.StoreStatus;
 import com.example.store.domain.store.service.StoreService;
 import java.time.LocalTime;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 @Business
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class StoreBusiness {
 
   private final StoreService storeService;
   private final StoreConverter storeConverter;
-  private final MessageConverter messageConverter;
 
-  public StoreRegisterResponse register(StoreRegisterRequest storeRegisterRequest, Long userId) {
+  /**
+   *
+   * @param storeRegisterRequest
+   * @param userId
+   * @return StoreRegisterResponse
+   * 스토어 등록
+   */
+  @Transactional(readOnly = false)
+  public void register(StoreRegisterRequest storeRegisterRequest, Long userId) {
 
     Store store = storeConverter.toEntity(storeRegisterRequest, userId);
 
     store.register();
     storeService.save(store);
 
-    RegisterImageRequest registerImageRequest = RegisterImageRequest.builder()
-        .storeId(store.getId())
-        .imageKind(ImageKind.STORE)
-        .serverName(storeRegisterRequest.getServerName())
-        .build();
-    storeService.publishRegisterImage(registerImageRequest);
+    RegisterImageRequest registerImageRequest =
+        RegisterImageRequest.of(
+            null,
+            store.getId(),
+            ImageKind.STORE,
+            storeRegisterRequest.getServerName()
+    );
 
-    return storeConverter.toResponse(store);
+    storeService.publishRegisterImage(registerImageRequest);
   }
 
-  public MessageResponse unregister(Long storeId, Long userId) {
+  /**
+   *
+   * @param storeId
+   * @param userId
+   * 스토어 삭제
+   */
+  public void unregister(Long storeId, Long userId) {
     Store targetStore = storeService.getStoreByIdAndUserId(storeId, userId)
-        .orElseThrow(() -> new IllegalArgumentException("STORE NOT FOUND"));
+        .orElseThrow(() -> new BusinessException(StoreErrorCode.STORE_NOT_FOUND));
 
     targetStore.unregister();
     storeService.delete(targetStore);
-    return messageConverter.toResponse("스토어가 삭제되었습니다.");
-
   }
 
-  public MessageResponse update(Long storeId, StoreUpdateRequest storeUpdateRequest) {
+  /**
+   *
+   * @param storeId
+   * @param storeUpdateRequest
+   * 스토어 수정
+   */
+  public void update(Long storeId, StoreUpdateRequest storeUpdateRequest) {
     Store targetStore = storeService.getStoreById(storeId)
-        .orElseThrow(() -> new IllegalArgumentException("STORE NOT FOUND"));
+        .orElseThrow(() -> new BusinessException(StoreErrorCode.STORE_NOT_FOUND));
 
     // 생각 중(수정)
-    targetStore.updateName(storeUpdateRequest.getName());
-    targetStore.updateAddress(storeUpdateRequest.getAddress());
-    targetStore.updatePhone(storeUpdateRequest.getPhone());
-    targetStore.updateCategory(storeUpdateRequest.getCategory());
-    targetStore.updateOperatingTime(storeUpdateRequest.getOperatingTime().getOpeningTime(),
-        storeUpdateRequest.getOperatingTime().getClosingTime());
+    targetStore.update(
+        storeUpdateRequest.name(),
+        storeUpdateRequest.address(),
+        storeUpdateRequest.phone(),
+        storeUpdateRequest.category(),
+        storeUpdateRequest.operatingTime().getOpeningTime(),
+        storeUpdateRequest.operatingTime().getClosingTime()
+    );
 
     storeService.save(targetStore);
 
-    UpdateImageRequest req = UpdateImageRequest.builder()
-        .storeId(targetStore.getId())
-        .imageKind(ImageKind.STORE)
-        .serverName(storeUpdateRequest.getServerName())
-        .build();
-    storeService.publishUpdateImage(req);
+    UpdateImageRequest updateImageRequest = UpdateImageRequest.of(
+        null,
+        targetStore.getId(),
+        ImageKind.STORE,
+        storeUpdateRequest.serverName()
+    );
 
-    return messageConverter.toResponse("스토어 정보가 수정되었습니다.");
+    storeService.publishUpdateImage(updateImageRequest);
   }
 
-  // 점주의 스토어 단건 조회
+  /**
+   *
+   * @param userId
+   * @return StoreSimpleResponse
+   * 외부 모듈에서 스토어 조회
+   */
   public StoreSimpleResponse getSimpleStore(Long userId) {
     List<Store> stores = storeService.getStoresByUserId(userId);
 
     if (stores.isEmpty()) {
-      throw new IllegalArgumentException("STORE NOT FOUND");
+      throw new BusinessException(StoreErrorCode.STORE_NOT_FOUND);
     }
 
     return storeConverter.toStoreSimpleResponse(stores);
   }
 
-  // 점주의 스토어 리스트 조회
-  public List<OwnerStoresResponse> getOwnerStore(Long userId) {
+  /**
+   *
+   * @param userId
+   * @return OwnerStoresResponse
+   * 점주의 스토어 리스트 조회
+   */
+  public OwnerStoresResponse getOwnerStore(Long userId) {
     List<Store> stores = storeService.getStoresByUserId(userId);
     if(stores.isEmpty()) {
-      throw new IllegalArgumentException("STORE NOT FOUND");
+      throw new BusinessException(StoreErrorCode.STORE_NOT_FOUND);
     }
 
     return storeConverter.toOwnerStoresResponse(stores);
@@ -109,45 +137,60 @@ public class StoreBusiness {
 
   public Store getStoreByIdAndUserId(Long storeId, Long userId) {
     return storeService.getStoreByIdAndUserId(storeId, userId)
-        .orElseThrow(() -> new IllegalArgumentException("STORE NOT FOUND"));
+        .orElseThrow(() -> new BusinessException(StoreErrorCode.STORE_NOT_FOUND));
   }
 
-  // 고객이 조회하는 스토어
-  public UserStoreResponse userStore(Long storeId) {
+  /**
+   *
+   * @param storeId
+   * @return UserStoreResponse
+   * 고객이 조회하는 스토어
+   */
+  public UserStoreResponse userFindStore(Long storeId) {
     Store store = storeService.getStoreById(storeId)
-        .orElseThrow(() -> new IllegalArgumentException("STORE NOT FOUND"));
+        .orElseThrow(() -> new BusinessException(StoreErrorCode.STORE_NOT_FOUND));
 
     return storeConverter.toUserStoreResponse(store);
   }
 
-  public List<StoreNameKeywordResponse> getStoresByNameKeyword(String name) {
+  /**
+   *
+   * @param name
+   * @return StoresNameKeywordResponse
+   * 스토어 키워드 검색
+   */
+  public StoresNameKeywordResponse getStoresByNameKeyword(String name) {
     if (name.isBlank()) {
-      throw new IsBlankException(StoreErrorCode.IS_BLANK);
-
+      throw new BusinessException(StoreErrorCode.IS_BLANK);
     }
 
     List<Store> stores = storeService.getStoresByNameKeyword(name);
     if (stores.isEmpty()) {
-      throw new IllegalArgumentException("STORE NOT FOUND");
+      throw new BusinessException(StoreErrorCode.STORE_NOT_FOUND);
     }
 
     return storeConverter.toStoreNameKeywordResponse(stores);
 
   }
 
-  // 주소 근처 && 영업 중인 스토어
-  public List<NearbyStoreResponse> getStoresByNearby(LocationRequest locationRequest) {
+  /**
+   *
+   * @param locationRequest
+   * @return NearbyStoresResponse
+   * 주소 근처 && 영업 중인 스토어 조회
+   */
+  public NearbyStoresResponse getStoresByNearby(LocationRequest locationRequest) {
     List<Store> nearbyStores = storeService.getStoresByRegionAndRadius(
         locationRequest.getSido(), locationRequest.getSigungu(),
         locationRequest.getEupMyeonDong(), locationRequest.getLatitude(),
         locationRequest.getLongitude(), locationRequest.getRadiusInMeters());
 
-    nearbyStores.stream().filter(
+    List<Store> openingNearByStore = nearbyStores.stream().filter(
         store -> store.getStoreStatus() == StoreStatus.REGISTERED
-        && store.getOperatingStatus() == OperatingStatus.OPEN
-    ).collect(Collectors.toList());
+            && store.getOperatingStatus() == OperatingStatus.OPEN
+    ).toList();
 
-    return storeConverter.toNearbyStoreResponse(nearbyStores, locationRequest.getRadiusInMeters());
+    return storeConverter.toNearbyStoreResponse(openingNearByStore, locationRequest.getRadiusInMeters());
   }
 
   public void updateStoreOpenClose() {
@@ -155,9 +198,8 @@ public class StoreBusiness {
     List<Store> stores = storeService.getStoreByDateTime(now);
 
     if (stores.isEmpty()) {
-      throw new IllegalArgumentException("STORE_NOT_FOUND");
+      throw new BusinessException(StoreErrorCode.STORE_NOT_FOUND);
     }
-
 
   }
 }
