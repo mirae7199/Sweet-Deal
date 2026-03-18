980a1f38c053e14ad1f48c2ff8d6971b1e013d77
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-03-18 19:12:11 +0900

    [SweetDeal] refactor: Api -> ResponseEntity 대체

--- store/src/main/java/com/example/store/domain/store/controller/StoreApiController.java
+++ store/src/main/java/com/example/store/domain/store/controller/StoreApiController.java
@@ -1,24 +1,23 @@
 package com.example.store.domain.store.controller;
 
-import com.example.global.anntation.UserSession;
-import com.example.global.api.Api;
+import com.example.global.annotation.UserSession;
 import com.example.global.resolver.User;
 import com.example.store.domain.store.business.StoreBusiness;
 import com.example.store.domain.store.controller.model.request.LocationRequest;
 import com.example.store.domain.store.controller.model.request.StoreRegisterRequest;
 import com.example.store.domain.store.controller.model.request.StoreUpdateRequest;
-import com.example.store.domain.store.controller.model.response.MessageResponse;
-import com.example.store.domain.store.controller.model.response.NearbyStoreResponse;
+import com.example.store.domain.store.controller.model.response.NearbyStoresResponse;
 import com.example.store.domain.store.controller.model.response.OwnerStoresResponse;
-import com.example.store.domain.store.controller.model.response.StoreNameKeywordResponse;
-import com.example.store.domain.store.controller.model.response.StoreRegisterResponse;
+import com.example.store.domain.store.controller.model.response.StoresNameKeywordResponse;
 import com.example.store.domain.store.controller.model.response.UserStoreResponse;
 import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -34,52 +33,68 @@ public class StoreApiController {
 
     public final StoreBusiness storeBusiness;
 
-    @PostMapping("/register") // 200
-    public Api<StoreRegisterResponse> register(@RequestBody @Valid StoreRegisterRequest storeRegisterRequest,
-        @Parameter(hidden = true) @UserSession User user) {
-        log.info("====================register");
+    @PostMapping // 200
+    public ResponseEntity<Void> register(
+        @RequestBody @Valid StoreRegisterRequest storeRegisterRequest,
+        @Parameter(hidden = true) @UserSession User user
+    ) {
 
-        StoreRegisterResponse response = storeBusiness.register(storeRegisterRequest, user.getId());
-        return Api.ok(response);
+        storeBusiness.register(storeRegisterRequest, user.getId());
+
+        log.info("=========store register=========");
+        return ResponseEntity.noContent().build();
     }
 
-    @PostMapping("/{storeId}/unregister") // 200
-    public Api<MessageResponse> unregister(@PathVariable Long storeId,
-        @Parameter(hidden = true) @UserSession User user) {
-        MessageResponse messageResponse = storeBusiness.unregister(storeId, user.getId());
-        return Api.ok(messageResponse);
+    @DeleteMapping("/{storeId}") // 200
+    public ResponseEntity<Void> unregister(
+        @PathVariable Long storeId,
+        @Parameter(hidden = true) @UserSession User user
+    ) {
+        storeBusiness.unregister(storeId, user.getId());
+        return ResponseEntity.noContent().build();
     }
 
-    @PostMapping("/{storeId}/update") // 200
-    public Api<MessageResponse> update(@PathVariable Long storeId,
-        @RequestBody @Valid StoreUpdateRequest storeUpdateRequest) {
-        MessageResponse messageResponse = storeBusiness.update(storeId, storeUpdateRequest);
-        return Api.ok(messageResponse);
+    @PatchMapping("/{storeId}") // 200
+    public ResponseEntity<Void> update(
+        @PathVariable Long storeId,
+        @RequestBody @Valid StoreUpdateRequest storeUpdateRequest
+    ) {
+        storeBusiness.update(storeId, storeUpdateRequest);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/owner") // 200
-    public Api<List<OwnerStoresResponse>> ownerStores(@Parameter(hidden = true) @UserSession User user) {
-        List<OwnerStoresResponse> ownerStores = storeBusiness.getOwnerStore(user.getId());
-        return Api.ok(ownerStores);
+    public ResponseEntity<OwnerStoresResponse> ownerStores(
+        @Parameter(hidden = true) @UserSession User user
+    ) {
+        OwnerStoresResponse ownerStores = storeBusiness.getOwnerStore(user.getId());
+        return ResponseEntity.ok(ownerStores);
     }
 
     @GetMapping("/{storeId}") // 200
-    public Api<UserStoreResponse> userStore(@PathVariable Long storeId) {
-        UserStoreResponse userStoreResponse = storeBusiness.userStore(storeId);
-        return Api.ok(userStoreResponse);
+    public ResponseEntity<UserStoreResponse> userStore(
+        @PathVariable Long storeId
+    ) {
+        UserStoreResponse userStoreResponse = storeBusiness.userFindStore(storeId);
+        return ResponseEntity.ok(userStoreResponse);
     }
 
     @GetMapping("/nearby")
-    public Api<List<NearbyStoreResponse>> nearby(@PathVariable @Valid LocationRequest locationRequest) {
-        List<NearbyStoreResponse> storesByNearby = storeBusiness.getStoresByNearby(locationRequest);
-        return Api.ok((storesByNearby));
+    public ResponseEntity<NearbyStoresResponse> nearby(
+        @PathVariable @Valid LocationRequest locationRequest
+    ) {
+        NearbyStoresResponse storesByNearby = storeBusiness.getStoresByNearby(locationRequest);
+        return ResponseEntity.ok((storesByNearby));
     }
 
     @GetMapping("/search") // 200
-    public Api<List<StoreNameKeywordResponse>> nameKeyword(@RequestParam String name) {
-        List<StoreNameKeywordResponse> stores = storeBusiness.getStoresByNameKeyword(
+    public ResponseEntity<StoresNameKeywordResponse> nameKeyword(
+        @RequestParam String name
+    ) {
+        StoresNameKeywordResponse storesByNameKeyword = storeBusiness.getStoresByNameKeyword(
             name);
-        return Api.ok(stores);
+
+        return ResponseEntity.ok(storesByNameKeyword);
     }
 
 }
