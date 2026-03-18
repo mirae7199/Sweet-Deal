512a8eaf73dbd42fdcbdffe602857f5ac5f40a0a
김미래 <79897894+mirae7199@users.noreply.github.com>
2026-03-18 19:18:27 +0900

    [SweetDeal] refactor: class -> record로 변경

--- store/src/main/java/com/example/store/domain/store/controller/model/request/StoreUpdateRequest.java
+++ store/src/main/java/com/example/store/domain/store/controller/model/request/StoreUpdateRequest.java
@@ -6,35 +6,22 @@ import com.example.store.domain.store.repository.enums.StoreCategory;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import java.util.List;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Builder
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class StoreUpdateRequest {
+public record StoreUpdateRequest(
+    @Pattern(
+        regexp = "^[가-힣A-Za-z0-9 ]{1,100}$",
+        message = "한글, 영문, 숫자 및 공백만 입력 가능합니다."
+    )
+    String name,
+    Address address,
+    @Size(min = 11, max = 11, message = "전화번호는 숫자 11자리여야 합니다.")
+    String phone,
+    StoreCategory category,
+    OperatingTime operatingTime,
+    List<String> serverName
+) {
 
-  @Pattern(
-      regexp = "^[가-힣A-Za-z0-9 ]{1,100}$",
-      message = "한글, 영문, 숫자 및 공백만 입력 가능합니다."
-  )
-  private String name;
-
-  private Address address;
-
-  @Size(min = 11, max = 11, message = "전화번호는 숫자 11자리여야 합니다.")
-  private String phone;
-
-//  @Size(min = 10, max = 10, message = "사업자등록번호는 숫자 10자리여야 합니다.")
-//  private String businessNumber;
-
-  private StoreCategory category;
-
-  private OperatingTime operatingTime;
-
-  private List<String> serverName;
 
 }
